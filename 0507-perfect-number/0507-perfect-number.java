8fc5386575ba5a0b1bd5d4b6ba8e3b03a23e5ab5
Prudhvi <prudhvigajjela8522@gmail.com>
2026-08-05 20:54:22 +0530

    Time: 0 ms (100%), Space: 42.2 MB (45.46%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean checkPerfectNumber(int n) {
+        if(n==6||n==28||n==496||n==8128||n==33550336){
+            return true;
+        }
+        else return false;
+    }
+}
